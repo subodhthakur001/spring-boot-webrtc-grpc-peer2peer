bc3508c50af592a678ac3e3bfd67683014aa86bf
subodh <skthakur.styles619@gmail.com>
2024-10-22 22:51:46 +0530

    proto buffer setup for grpc

---
+++ src/main/java/com/example/WebRTCServer/WebRtcServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebRTCServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRtcServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebRTCServer/WebRtcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebRTCServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
